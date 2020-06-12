4af03e32b2a9ea707bb40d2d5f43752ab95ee7b6
hirosyos <mu33devel1@gmail.com>
2020-06-12 14:00:44 +0900

    GRAM呼び出しjavascript追加

--- php/gram_read.php
+++ php/gram_read.php
@@ -18,9 +18,9 @@ $status = $stmt->execute();
 //タイトル作成
 $title = "";
 if ($_SESSION["is_admin"] == 1) {
-  $title = "GRAMリスト（一覧画面）[users_id:{$_SESSION["users_id"]}][管理者]";
+  $title = "GRAMリスト（一覧画面）[ユーザID:{$_SESSION["users_id"]}][管理者]";
 } else {
-  $title = "GRAMリスト（一覧画面）[users_id:{$_SESSION["users_id"]}][一般]";
+  $title = "GRAMリスト（一覧画面）[ユーザID:{$_SESSION["users_id"]}][一般]";
 }
 
 // データ登録処理後
@@ -36,8 +36,10 @@ if ($status == false) {
   $output = "";
   // <tr><td>deadline</td><td>todo</td><tr>の形になるようにforeachで順番に$outputへデータを追加
   // `.=`は後ろに文字列を追加する，の意味
+  $valCnt = 1;
   foreach ($result as $record) {
     $output .= "<tr>";
+    $output .= "<td><input type='checkbox' class='checks' value='{$valCnt}'></td>";
     $output .= "<td>{$record["id"]}</td>";
     $output .= "<td>{$record["users_id"]}</td>";
     $output .= "<td>{$record["last_name"]}</td>";
@@ -102,6 +104,7 @@ if ($status == false) {
     <table>
       <thead>
         <tr>
+          <th>CHECK</th>
           <th>ID</th>
           <th>ユーザID</th>
           <th>名字</th>
@@ -130,10 +133,142 @@ if ($status == false) {
       </tbody>
     </table>
   </fieldset>
-</body>
 
-<footer>
-  <p>統一期とは東京DEVの期を基準とし、東京LABは7期、福岡DEVは10期、福岡LABは13期足し合わせたものである</p>
-</footer>
+  <button id="readGsGram">OPEN GRAM</button>
+  <table id='gsGramTable'></table>
+
+  <footer>
+    <p>統一期とは東京DEVの期を基準とし、東京LABは7期、福岡DEVは10期、福岡LABは13期足し合わせたものである</p>
+  </footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script>
+    //東京DEVを基準とした時の差分
+    const gsDevLabGap = {
+      tokyoDev: 0,
+      tokyoLav: 7,
+      fukuokaDev: 10,
+      fukuokaLav: 13
+    };
+
+    //
+    //JQueryの始まり
+    //
+    $(function() {
+
+      //
+      // 名簿読み出しクリック時
+      //
+      // $('#readListBtn').on('click', function() {
+      //   let createList = '';
+      //   createList = '<tr>';
+      //   createList += '<th>check</th>';
+      //   createList += '<th>苗字</th>';
+      //   createList += '<th>名前</th>';
+      //   createList += '<th>ミョウジ</th>';
+      //   createList += '<th>ナマエ</th>';
+      //   createList += '<th>ニックネーム</th>';
+      //   createList += '<th>東京/福岡</th>';
+      //   createList += '<th>DIV/LAB</th>';
+      //   createList += '<th>期</th>';
+      //   createList += '<th>統一期</th>';
+      //   createList += '</tr>';
+      //   for (let i = 0; i < gsMemberList.length; i++) {
+      //     createList += '<tr>';
+      //     createList += '<td><input type="checkbox" class="checks" value=' + i + '></td>';
+      //     createList += '<td>' + gsMemberList[i].lastName + '</td>';
+      //     createList += '<td>' + gsMemberList[i].firstName + '</td>';
+      //     createList += '<td>' + gsMemberList[i].lastNameKana + '</td>';
+      //     createList += '<td>' + gsMemberList[i].firstNameKana + '</td>';
+      //     createList += '<td>' + gsMemberList[i].nickName + '</td>';
+      //     createList += '<td>' + gsMemberList[i].gsPlace + '</td>';
+      //     createList += '<td>' + gsMemberList[i].gsDevLabKind + '</td>';
+      //     createList += '<td>' + gsMemberList[i].gsDevLavNo + '</td>';
+      //     //統一期を求める
+      //     let gsDevLavUniNo = 0;
+      //     if (gsMemberList[i].gsPlace == '福岡' && gsMemberList[i].gsDevLabKind == 'LAB') {
+      //       //福岡LABは東京DEVに対して13期遅れ
+      //       gsMemberList[i].gsDevLavUniNo = Number(gsMemberList[i].gsDevLavNo) + 13;
+      //     } else if (gsMemberList[i].gsPlace == '福岡' && gsMemberList[i].gsDevLabKind == 'DEV') {
+      //       //福岡DEVは東京DEVに対して10期遅れ
+      //       gsMemberList[i].gsDevLavUniNo = Number(gsMemberList[i].gsDevLavNo) + 10;
+      //     } else if (gsMemberList[i].gsPlace == '東京' && gsMemberList[i].gsDevLabKind == 'LAB') {
+      //       //東京LABは東京DEVに対して7期遅れ
+      //       gsMemberList[i].gsDevLavUniNo = Number(gsMemberList[i].gsDevLavNo) + 7;
+      //     } else {
+      //       //東京DEVからスタートしたので統一期は東京DEV基準
+      //       gsMemberList[i].gsDevLavUniNo = Number(gsMemberList[i].gsDevLavNo)
+      //     }
+      //     createList += '<td>' + gsMemberList[i].gsDevLavUniNo + '</td>';
+      //     createList += '</tr>';
+      //   }
+      //   // 文字列を#gsTable
+      //   $('#gsTable').html(createList)
+
+      // });
+
+
+      //
+      // ジーズグラム読み出しクリック時
+      //
+      $('#readGsGram').on('click', function() {
+        let checks = document.getElementsByClassName('checks');
+        let str = '';
+        // 誰がジーズグラム対象かをリストアップする
+        let gsMemberListNo = [];
+        for (let i = 0; i < gsMemberList.length; i++) {
+          if (checks[i].checked === true) {
+            gsMemberListNo.push(i);
+          }
+        }
+
+        //一番上の行はそれぞれの行の人物に対する人物を配置
+        let createList = '';
+        createList = '<tr>';
+        createList += '<th></th>';
+        for (let i = 0; i < gsMemberListNo.length; i++) {
+          createList += '<th>' + gsMemberList[gsMemberListNo[i]].nickName + '</th>';
+        }
+        createList += '</tr>';
+
+        //2番目以降の行でジーズグラムを構成
+        for (let i = 0; i < gsMemberListNo.length; i++) {
+          createList += '<tr>';
+          createList += '<td>' + gsMemberList[gsMemberListNo[i]].nickName + '</td>';
+
+          for (let j = 0; j < gsMemberListNo.length; j++) {
+            let Sa;
+            createList += '<td>'
+            if (gsMemberList[gsMemberListNo[i]].gsDevLavUniNo > gsMemberList[gsMemberListNo[j]].gsDevLavUniNo) {
+              //自分の方が後輩 相手の方が先輩
+              Sa = gsMemberList[gsMemberListNo[i]].gsDevLavUniNo - gsMemberList[gsMemberListNo[j]].gsDevLavUniNo + '期先輩';
+            } else if (gsMemberList[gsMemberListNo[i]].gsDevLavUniNo < gsMemberList[gsMemberListNo[j]].gsDevLavUniNo) {
+              //自分の方が先輩 相手の方が後輩
+              Sa = gsMemberList[gsMemberListNo[j]].gsDevLavUniNo - gsMemberList[gsMemberListNo[i]].gsDevLavUniNo + '期後輩';
+            } else if (i == j) {
+              //自分
+              Sa = '／';
+            } else {
+              //同期
+              Sa = '同期';
+            }
+            createList += '<p>' + Sa + '</p>';
+            createList += '<p>' + gsMemberList[gsMemberListNo[i]].gsGram[gsMemberListNo[j]] + '</p>';
+            createList += '</td>'
+          }
+
+          createList += '</tr>';
+        }
+        // 文字列を#gsTable
+        $('#gsGramTable').html(createList)
+
+
+      });
+
+
+    });
+  </script>
+
+</body>
 
 </html>
